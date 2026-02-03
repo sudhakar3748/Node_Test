fbdee0c2932fd3722494b4f365309c948bf9d70f
Sudhakar Verma <Vermasudhakar32@gmail.com>
2026-02-03 11:57:55 +0530

    updated

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,3 +1,29 @@
 const fs = require('fs');
 
-fs.writeFileSync("./index.txt" , 'Hello World');
+// fs.writeFileSync("./index.txt" , 'Hello World');
+// fs.writeFileSync('./abes.text' , 'Hello ABES');
+
+// fs.writeFileSync("./a1.txt" , 'First File');
+// fs.writeFileSync('./a2.text' , 'Second File');
+
+// fs.readFileSync('./abes.txt' , 'utf-8');
+
+// const result = fs.readFileSync("./abes.txt" , "utf-8");
+// console.log(result);
+
+// const result = fs.readFileSync("./index.txt" , "utf-8");
+// console.log(result);
+
+// fs.writeFile("./abes.text" , "we are code" , () => { });
+
+// fs.readFile("./abes.text" , "utf-8" , (err, result) => {
+//     if (err){
+//         console.log("error " , err);
+//     }
+//     else{
+//         console.log(result);
+//     }
+// } );
+
+
+

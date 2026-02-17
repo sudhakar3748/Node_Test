fa0b2278291cf6be129bb0fa011457bb07357aa7
Sudhakar Verma <Vermasudhakar32@gmail.com>
2026-02-17 11:40:50 +0530

    update

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+// const fs = require('fs');
 
 // fs.writeFileSync("./index.txt" , 'Hello World');
 // fs.writeFileSync('./abes.text' , 'Hello ABES');
@@ -30,4 +30,22 @@ const fs = require('fs');
 
 // fs.appendFile("./abes.text" , " and My name is Sudhakar Verma" , () => {});
 
-fs.cpSync("./abes.txt" , "./a1.txt");
+// fs.cpSync("./abes.txt" , "./a1.txt");
+
+// fs.unlinkSync("./abes.txt");
+
+// fs.unlink("./abes.txt" , () => {
+    
+// } );
+
+const os = require('os');
+console.log("System platform" , os.platform());
+console.log("user info" , os.userInfo());
+console.log(" cpu" , os.arch());
+console.log(" free memory" , os.freemem());
+console.log(" total memory" , os.totalmem());
+console.log(" uptime" , os.uptime());
+console.log(" homedir" , os.homedir());
+console.log(" host name" , os.hostname ());
+
+
